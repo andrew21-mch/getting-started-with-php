acaa6fec097b17ea5b9994c93e3bd19754ac199a
andrew21-mch <nfonandrew73@gmail.com>
2022-05-23 04:09:26 +0100

    data types

---
+++ data_types/datatypes.php
@@ -0,0 +1,29 @@
+<?php
+
+//integer
+
+echo "Integer Values <br>";
+$x = 7;
+$y = -8;
+
+var_dump($x);
+var_dump($y);
+
+echo "<br>";
+echo "Float Values <br>";
+
+//float
+$float1 = 7.5;
+$float2 = -8.5;
+
+var_dump($float1);
+var_dump($float2);
+
+//Strings
+$string1 = "Hello World";
+$string2 = 'Hello World';
+
+var_dump($string1);
+
+$bool = true;
+var_dump($bool);
